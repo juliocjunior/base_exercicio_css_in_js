172acaa3be0e5e7b8c76ee591e696dad1fab5bfc
Julio Vieira <juliocesarjuniorjja@hotmail.com>
2023-10-19 09:52:05 -0300

    criei os arquivos styles.ts com a estilização de cada componente e deletei os arquivos css.modules

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { Cabecalhoo } from './styles'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <Cabecalhoo>
     <h1>EBAC Jobs</h1>
-  </header>
+  </Cabecalhoo>
 )
 
 export default Cabecalho
---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Cabecalhoo = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
+// .cabecalho {
+//   background-color: var(--cor-secundaria);
+// eslint-disable-next-line prettier/prettier
+//   color: var(--cor-principal);
+//   text-align: center;
+//   padding: 24px 0;
+// }
